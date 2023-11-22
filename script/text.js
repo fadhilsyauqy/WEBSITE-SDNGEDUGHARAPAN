43b7162c993e73788cacbeca593a254a7a6b4f8e
Fadhil Syauqy <rahmatfadhilsauqy08@gmail.com>
2023-11-22 13:01:38 +0700

    commit js

---
+++ script/text.js
@@ -0,0 +1,18 @@
+import Letterize from "https://cdn.skypack.dev/letterizejs@2.0.0";
+const test = new Letterize({
+    targets: "#animateMe",
+});
+
+var animation = anime.timeline({
+    targets: test.listAll,
+    delay: anime.stagger(50),
+    loop: true,
+});
+
+animation
+    .add({
+        translateY: -40,
+    })
+    .add({
+        translateY: 0,
+    });
